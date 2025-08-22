fa0d2adec17a243afb3cc344fd4508b84b0a3bc7
lairongcheng <10856303@qq.com>
2025-08-22 10:23:53 +0800

    Revert "企业微信"
    This reverts commit 7d823cdcd0063040de87b754ad93c8439d153628.

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,9 @@ import { NConfigProvider } from 'naive-ui'
 import { NaiveProvider } from '@/components/common'
 import { useTheme } from '@/hooks/useTheme'
 import { useLanguage } from '@/hooks/useLanguage'
-import { onMounted } from 'vue'
-import VConsole from 'vconsole'
-
 
 const { theme, themeOverrides } = useTheme()
 const { language } = useLanguage()
-
- onMounted(() => {
-  new VConsole();
-})
 </script>
 
 <template>
